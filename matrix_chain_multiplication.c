91a2a03ad5b82558a2fc99bb679fe308c6507954
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-03-06 01:12:13 +0530

    tough

--- matrix_chain_multiplication.c
+++ matrix_chain_multiplication.c
@@ -19,5 +19,16 @@ int matrix_chain_order(int d[],int i,int j){
     }
 }
 int main(){
+    int n,i;
+    printf("\n Enter the number of matrices that we have to multiply:");
+    scanf("%d",&n);
+    int dim[n+1];
+    printf("\n Enter the dimensions of the matrices(space separated Values):");
+    for(i=0;i<=n;i++){
+        scanf("%d",&dim[i]);
+    }
+    int min_multiplications = matrix_chain_order(dim,1,n);
+    printf("Minimum number of scalar multiplications: %d\n", min_multiplications);
+
     return 0;
 }
