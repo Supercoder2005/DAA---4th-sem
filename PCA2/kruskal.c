212731e442a5334c84d1768aa1aef85e76f93b6d
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-04-28 14:07:15 +0000

    not done kruskal

--- PCA2/kruskal.c
+++ PCA2/kruskal.c
@@ -6,8 +6,8 @@ int edge[MAX][3]; // edge is such an array contains rows = total e edges , col =
 int parent[MAX]; // which are used to store parent vertex of each vertices 
 
 int findParent(int v){
-    if(parent[v] != v){
-        parent[v] = v;
+    while(parent[v] != v){
+        v = parent[v];
     }
     return v;
 }
@@ -17,7 +17,7 @@ void sortEdges(){
             if(edge[i][2]>edge[j][2]){
                 int temp1 = edge[i][0];
                 int temp2 = edge[i][1];
-                int temp3 = edge[i][3];
+                int temp3 = edge[i][2];
 
                 edge[i][0] = edge[j][0];
                 edge[i][1] = edge[j][0];
@@ -31,6 +31,36 @@ void sortEdges(){
     }
 }
 
+int kruskal(){
+    int totalCost = 0;
+    int count = 0;
+
+    sortEdges();
+
+    for(int i=0;i<e;i++){
+        int u = edge[i][0];
+        int v = edge[i][1];
+        int weight = edge[i][2];
+
+        int rootU = findParent(u);
+        int rootV = findParent(v);
+
+        if(rootU != rootV){
+            parent[rootV] = rootU;
+        }
+
+        printf("%d -> %d: Weight %d\n",u,v,weight);
+        totalCost += weight;
+        count ++;
+
+        if(count==n-1){
+            break;
+        }
+    }
+    return totalCost;
+
+}
+
 int main(){
     printf("\n Enter the no of vertices:");
     scanf("%d",&n);
@@ -38,12 +68,13 @@ int main(){
     scanf("%d",&e);
     printf("\n Enter the edges (u -> v weight):\n");
     for(int i=0;i<e;i++){
-        for(int j=0;j<3;j++){
-            scanf("%d %d %d",&edge[i][0],&edge[i][1],&edge[i][2]);
-        }
+        scanf("%d %d %d",&edge[i][0],&edge[i][1],&edge[i][2]);
     }
     // initialize the parent array , initially each vertex is their own parent 
     for(int v=0;v<n;v++){
         parent[v] = v;
     }
+    int v;
+    printf("\n The total cost of the MST : %d",kruskal());
+    return 0;
 }
